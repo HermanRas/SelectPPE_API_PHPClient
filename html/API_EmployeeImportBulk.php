2127287c06ecbfa0556e8d66d012e77d0e6b35bb
Herman Ras <herman.ras@petradiamonds.com>
2024-06-27 16:24:36 +0200

    updated SQL fields

--- html/API_EmployeeImportBulk.php
+++ html/API_EmployeeImportBulk.php
@@ -2,6 +2,9 @@
 //Import CURL Function
 require_once('config/_CURL.php');
 
+// Set API Defaults
+require_once('config/db.php');
+
 // Fetch SQL Data
 require("SQL_GetEmpData.php"); //returns $empDataRS
 
@@ -14,29 +17,29 @@ for ($i = 0; $i < count($empDataRS[0]); $i++) {
     $request['apiKey'] = "d371009e-1c3f-47cc-8a99-7915a4319ff3";
     $request['clientKey'] = "f7bb6e25-15a3-4a2a-bf67-a0b565c95a75";
     $request['echo'] = false;
-    $request['data'][$i]['employeeCode'] = "$rec[employeeCode]";
+    $request['data'][$i]['employeeCode'] = "$rec[employeecode]";
     $request['data'][$i]['erpNr'] = "$rec[erpNr]";
     $request['data'][$i]['badgeNr'] = "$rec[badgeNr]";
     $request['data'][$i]['surname'] = "$rec[surname]";
     $request['data'][$i]['initials'] = "$rec[initials]";
     $request['data'][$i]['title'] = "$rec[title]";
     $request['data'][$i]['gender'] = "$rec[gender]";
-    $request['data'][$i]['idNumber'] = "$rec[idNumber]";
+    $request['data'][$i]['idNumber'] = "$rec[idnumber]";
     $request['data'][$i]['occupation'] = "$rec[occupation]";
     $request['data'][$i]['workType'] = "$rec[workType]";
     $request['data'][$i]['status'] = "$rec[status]";
     $request['data'][$i]['employeeType'] = "$rec[employeeType]";
     $request['data'][$i]['contractor'] = "$rec[contractor]";
     $request['data'][$i]['eligible'] = "$rec[eligible]";
-    $request['data'][$i]['site'] = "$rec[site]";
+    $request['data'][$i]['site'] = "$rec[Site]";
     $request['data'][$i]['operationType'] = "$rec[operationType]";
     $request['data'][$i]['location'] = "$rec[location]";
     $request['data'][$i]['function'] = "$rec[function]";
     $request['data'][$i]['personnelArea'] = "$rec[personnelArea]";
     $request['data'][$i]['personnelSubarea'] = "$rec[personnelSubarea]";
     $request['data'][$i]['altSite'] = "$rec[altSite]";
-    $request['data'][$i]['email'] = "$rec[email]";
-    $request['data'][$i]['cellNr'] = "$rec[cellNr]";
+    $request['data'][$i]['email'] = "$rec[EmailAddress]";
+    $request['data'][$i]['cellNr'] = "$rec[CellNumber]";
     $request['data'][$i]['jobLevel'] = "$rec[jobLevel]";
     $request['data'][$i]['manager'] = "$rec[manager]";
     $request['data'][$i]['managerEmployeeCode'] = "$rec[managerEmployeeCode]";
@@ -48,10 +51,14 @@ for ($i = 0; $i < count($empDataRS[0]); $i++) {
     $request['data'][$i]['thirdCC'] = "$rec[thirdCC]";
     $request['data'][$i]['fourthCC'] = "$rec[fourthCC]";
 }
+
 //DEBUG
-echo json_encode($request);
-die;
+// echo json_encode($request);
+// die;
 
+$HEAD_Data =  [
+    'Content-Type: application/json; charset=utf-8',
+];
 
-$PostResult = curl('/api/Employee/EmployeeImportBulk', $HEAD_Data, $_POST);
+$PostResult = curl($BaseURL . '/api/Employee/EmployeeImportBulk', $HEAD_Data, $_POST);
 var_dump($PostResult);
